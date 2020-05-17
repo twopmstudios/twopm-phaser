b753f4d2eb16aebbd68fa8f1e60eb54315ce11f8
Ben Follington <5009316+bfollington@users.noreply.github.com>
2020-05-17 15:39:15 +1000

    Remove hack for Phaser import
    I prefer self-declaring globals to inconsistent type information

--- src/config.ts
+++ src/config.ts
@@ -1,5 +1,3 @@
-import Phaser from 'phaser'
-
 export default {
   type: Phaser.AUTO,
   parent: 'game',
--- src/scenes/Game.ts
+++ src/scenes/Game.ts
@@ -1,4 +1,3 @@
-import Phaser from 'phaser'
 
 export default class Demo extends Phaser.Scene {
   logo: Phaser.GameObjects.Image
